808bc48433ae1b31381387d76ab1c665b3095bba
Dmitry <moskovenko.dmitry@yandex.ru>
2021-06-18 01:57:13 +0300

    complete view logic

--- ARImageFinder/ARImageFinder/ViewController.swift
+++ ARImageFinder/ARImageFinder/ViewController.swift
@@ -10,131 +10,131 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+  
+  @IBOutlet var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-      
-        sceneView.autoenablesDefaultLighting = true
-        
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-//        sceneView.scene = scene
-    }
+    // Set the view's delegate
+    sceneView.delegate = self
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-      let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
-      
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.detectionImages = referenceImages
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
+    sceneView.autoenablesDefaultLighting = true
     
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-      switch anchor {
-      case let imageAnchor as ARImageAnchor:
-        nodeAdded(node, for: imageAnchor)
-      case let planeAnchor as ARPlaneAnchor:
-        nodeAdded(node, for: planeAnchor)
-      default:
-        print("An anchor was discoveres, but it is not for planes or images")
-      }
-    }
+    // Create a new scene
+    //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    
+    // Set the scene to the view
+    //        sceneView.scene = scene
+  }
   
-    func nodeAdded(_ node: SCNNode, for imageAnchor: ARImageAnchor) {
-      let referenceImage = imageAnchor.referenceImage
-      
-      let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
-      let planeNode = SCNNode(geometry: plane)
-      planeNode.name = "plane"
-      planeNode.opacity = 0.0
-      planeNode.eulerAngles.x = -Float.pi / 2
-      
-      switch referenceImage.name {
-      case "troyka":
-        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-        let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
-        guard let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false) else { return }
-        shipNode.scale = SCNVector3(0.07, 0.07, 0.07)
-        shipNode.opacity = 0.0
-        node.addChildNode(shipNode)
-        shipNode.runAction(fadeInAction)
-        print("Created node related to \"troyka.png\" image")
-      case "eleсtro":
-        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        let sphereNode = createSphere(0.02)
-        node.addChildNode(sphereNode)
-        sphereNode.runAction(fadeInAction)
-        print("Created node related to \"electro.jpg\" image")
-      default:
-        planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
-        print("Created node related to another image")
-      }
-      
-      node.addChildNode(planeNode)
-      planeNode.runAction(fadeOpacityAction)
-    }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+    
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
+    configuration.detectionImages = referenceImages
+    
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
   
-    func createSphere(_ radius: CGFloat) -> SCNNode {
-      let sphere = SCNNode(geometry: SCNSphere(radius: radius))
-      sphere.geometry?.firstMaterial?.lightingModel = .physicallyBased
-      sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-      sphere.opacity = 0.0
-      sphere.position = SCNVector3(0.0, 0.02, 0.0)
-      return sphere
-    }
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // Pause the view's session
+    sceneView.session.pause()
+  }
   
-    func nodeAdded(_ node: SCNNode, for planeAnchor: ARPlaneAnchor) {
-
-    }
+  // MARK: - ARSCNViewDelegate
   
-    var fadeOpacityAction: SCNAction {
-      return .sequence([.fadeOpacity(to: 0.5, duration: 2.0)])
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    switch anchor {
+    case let imageAnchor as ARImageAnchor:
+      nodeAdded(node, for: imageAnchor)
+    case let planeAnchor as ARPlaneAnchor:
+      nodeAdded(node, for: planeAnchor)
+    default:
+      print("An anchor was discoveres, but it is not for planes or images")
     }
+  }
   
-    var fadeInAction: SCNAction {
-      return .sequence([.fadeIn(duration: 2.0)])
-    }
-  
-    var waitRemoveAction: SCNAction {
-      return .sequence([.fadeOut(duration: 2.0), .removeFromParentNode()])
-    }
-  
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+  func nodeAdded(_ node: SCNNode, for imageAnchor: ARImageAnchor) {
+    let referenceImage = imageAnchor.referenceImage
+    
+    let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+    let planeNode = SCNNode(geometry: plane)
+    planeNode.name = "plane"
+    planeNode.opacity = 0.0
+    planeNode.eulerAngles.x = -Float.pi / 2
+    
+    switch referenceImage.name {
+    case "troyka":
+      planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+      let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
+      guard let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false) else { return }
+      shipNode.scale = SCNVector3(0.07, 0.07, 0.07)
+      shipNode.opacity = 0.0
+      node.addChildNode(shipNode)
+      shipNode.runAction(fadeInAction)
+      print("Created node related to \"troyka.png\" image")
+    case "eleсtro":
+      planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+      let sphereNode = createSphere(0.02)
+      node.addChildNode(sphereNode)
+      sphereNode.runAction(fadeInAction)
+      print("Created node related to \"electro.jpg\" image")
+    default:
+      planeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
+      print("Created node related to another image")
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
+    node.addChildNode(planeNode)
+    planeNode.runAction(fadeOpacityAction)
+  }
+  
+  func createSphere(_ radius: CGFloat) -> SCNNode {
+    let sphere = SCNNode(geometry: SCNSphere(radius: radius))
+    sphere.geometry?.firstMaterial?.lightingModel = .physicallyBased
+    sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+    sphere.opacity = 0.0
+    sphere.position = SCNVector3(0.0, 0.02, 0.0)
+    return sphere
+  }
+  
+  func nodeAdded(_ node: SCNNode, for planeAnchor: ARPlaneAnchor) {
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+  }
+  
+  var fadeOpacityAction: SCNAction {
+    return .sequence([.fadeOpacity(to: 0.5, duration: 2.0)])
+  }
+  
+  var fadeInAction: SCNAction {
+    return .sequence([.fadeIn(duration: 2.0)])
+  }
+  
+  var waitRemoveAction: SCNAction {
+    return .sequence([.fadeOut(duration: 2.0), .removeFromParentNode()])
+  }
+  
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    // Present an error message to the user
+    
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+  }
 }
--- ModelPicker/ModelPicker/ContentView.swift
+++ ModelPicker/ModelPicker/ContentView.swift
@@ -9,35 +9,109 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
-    var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+  @State private var isPlacementEnabled = false
+  
+  private var models: [String] = {
+    // Dynamically get model filenames
+    let filemanager = FileManager.default
+    guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {
+      return []
     }
+    
+    var avalibleModels: [String] = []
+    for filename in files where filename.hasSuffix("usdz") {
+      let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+      avalibleModels.append(modelName)
+    }
+    return avalibleModels
+  }()
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+//      ARViewContainer()
+      
+      if self.isPlacementEnabled {
+        PlacementButtonsView()
+      } else {
+        ModelPickerView(models: self.models)
+      }
+    }
+  }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+  
+  func makeUIView(context: Context) -> ARView {
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
+    let arView = ARView(frame: .zero)
+    
+    return arView
+    
+  }
+  
+  func updateUIView(_ uiView: ARView, context: Context) {}
+  
+}
+
+struct ModelPickerView: View {
+  var models: [String]
+  
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(spacing: 30) {
+        ForEach(0 ..< self.models.count) {
+          index in
+          Button(action: { print("DEBUG: selected model with name: \(self.models[index]).")
+          }) {
+            Image(uiImage: UIImage(named: self.models[index])!)
+              .resizable()
+              .frame(height: 80)
+              .aspectRatio(1, contentMode: .fit)
+              .background(Color.white)
+              .cornerRadius(12)
+          }
+          .buttonStyle(PlainButtonStyle())
+        }
+      }
     }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    .padding(20)
+    .background(Color.black.opacity(0.5))
+  }
+}
+
+struct PlacementButtonsView: View {
+  var body: some View {
+    HStack {
+      // Cancel button
+      Button(action: {
+        print("DEBUG: model placement canceled.")
+      }) {
+        Image(systemName: "xmark")
+          .frame(width: 60, height: 60)
+          .font(.title)
+          .background(Color.white.opacity(0.75))
+          .cornerRadius(30)
+          .padding(20)
+      }
+      // Confirm button
+      Button(action: {
+        print("DEBUG: model placement confirmed.")
+      }) {
+        Image(systemName: "checkmark")
+          .frame(width: 60, height: 60)
+          .font(.title)
+          .background(Color.white.opacity(0.75))
+          .cornerRadius(30)
+          .padding(20)
+      }
+    }
+  }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
